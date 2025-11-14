a3a85949f3d0f2e70ece6eafe4977bbbbca6ded7
INTROLLER <pv.levap@gmail.com>
2025-11-14 11:52:04 +0100

    Update

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import bcrypt
+import time
 
-def hash_password(password):
-  salt = bcrypt.gensalt()
+def hash_password(password, rounds=12):
+  salt = bcrypt.gensalt(rounds=rounds)
   hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
   return hashed_password
 
-print(hash_password("password"))
-salt = bcrypt.gensalt()
+start = time.time()
+hash = hash_password("password")
+stop = time.time()
+print(hash, stop - start)
 
-print(salt)
+
+start2 = time.time()
+hash = hash_password("password", 4)
+stop2 = time.time()
+print(hash, stop2 - start2)
+print((stop - start)/(stop2 - start2))
