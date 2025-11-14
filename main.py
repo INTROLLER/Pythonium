7a4efc1e7ff12b0b6f05afa6ecea7c90a491d015
INTROLLER <pv.levap@gmail.com>
2025-11-14 11:33:47 +0100

    Update

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import bcrypt
+
+def hash_password(password):
+  salt = bcrypt.gensalt()
+  hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+  return hashed_password
+
+print(hash_password("password"))
