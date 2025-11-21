f3d3d8061e02ae0325e40322cd847b414ddd9813
ace-of-spades07 <pzetterling@gmail.com>
2025-11-21 11:43:53 +0100

    Update

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+# Bibliotek
 import bcrypt
 import time
 import string
 import itertools
 from hashlib import sha256
 
+# Alla tecken
 chars = string.printable
 chars = chars.strip("\n")
 chars = chars.strip()
 
+# Hasha lösenord
 def hash_password(password):
   hashed_password = sha256(password.encode('utf-8')).hexdigest()
   return hashed_password
 
-def brute_force(hash, slice, charcount):
+# Ren brute force-attack (hash är inputlösenord, slice är antal tecken från chars som ska vara med)
+def brute_force(hash, slice):
   global chars
   chars = chars[:slice]
-  guess = []
-  for i in range(charcount):
-    guess.append(chars[0])
-  print("".join(str(x) for x in guess))
+  guesses = 0
 
+  for i in range(1, 10):
+    print("Testing all passwords with " + str(i) + " characters")
+    combination_list = itertools.product(chars, repeat=i)
 
+    for combination in combination_list:
+      hashed = hash_password("".join(str(x) for x in combination))
+
+      # Progress-uppdatering för att se till att datorn inte går sönder
+      guesses += 1
+      if guesses % 1000000 == 0:
+        print("Currently guessed " + str(guesses) + " passwords")
+
+      # Avbryt funktion om lösenord hittas
+      if hashed == hash:
+        print("Found password: " + "".join(str(x) for x in combination))
+        return
+      
+  # Lösenord hittades inte
+  print("Couldn't find password")
+
+  
+# Skicka in lösenord och kör brute force
 password = input("Password: ")
 hash = hash_password(password)
-combination_list = itertools.product(chars, repeat=4)
-
-for combination in combination_list:
-  hashed = hash_password("".join(str(x) for x in combination))
-  if hashed == hash:
-    print("Found password: " + "".join(str(x) for x in combination))
-    break
-# password = input("Enter password: ")
-list = itertools.product(chars, repeat=3)
-for i in list:
-  print("".join(i))
+brute_force(hash, 94)
