cecc6e258ebd284568010f0b7ec69671cc608607
INTROLLER <pv.levap@gmail.com>
2025-11-21 11:18:03 +0100

    Update

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ chars = string.printable
 chars = chars.strip("\n")
 chars = chars.strip()
 
-print(len(chars))
-print(chars)
-
 def hash_password(password):
   hashed_password = sha256(password.encode('utf-8')).hexdigest()
   return hashed_password
@@ -24,10 +21,12 @@ def brute_force(hash, slice, charcount):
   print("".join(str(x) for x in guess))
 
 
-password = input("Enter password: ")
-print(hash_password(password))
-hash = hash_password("password")
-print(hash)
+password = input("Password: ")
+hash = hash_password(password)
+combination_list = itertools.product(chars, repeat=4)
 
-hash = hash_password("password")
-print(hash)
+for combination in combination_list:
+  hashed = hash_password("".join(str(x) for x in combination))
+  if hashed == hash:
+    print("Found password: " + "".join(str(x) for x in combination))
+    break
