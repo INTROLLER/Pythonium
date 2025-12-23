2009f0ce9515f2c9b8a0796c88fcc35cd33ebeb5
ace-of-spades07 <pzetterling@gmail.com>
2025-12-23 15:05:42 +0100

    Buggar fixade

--- main.py
+++ main.py
@@ -16,17 +16,16 @@ digits = string.digits
 special_characters = string.punctuation
 
 hash_types = ["sha256", "bcrypt", "md5"]
-hash_type = None
+hash_type = int()
 
 # Hasha lösenord
-def hash_password(password, hash=hash_type):
-  hashed_password = None
+def hash_password(password, algo):
 
-  if hash == "sha256":
+  if algo == "sha256":
     hashed_password = sha256(password.encode('utf-8')).hexdigest()
-  elif hash == "bcrypt":
+  elif algo == "bcrypt":
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=4))
-  elif hash == "md5":
+  elif algo == "md5":
     hashed_password = md5(password.encode('utf-8')).hexdigest()
 
   return hashed_password
@@ -41,30 +40,43 @@ def brute_force(hash, slice):
   for i in range(1, 10):
     print("Testing all passwords with " + str(i) + " characters")
     combination_list = itertools.product(chars, repeat=i)
+    interval = 0
+    subtract = start
 
     for combination in combination_list:
-      hashed = hash_password("".join(str(x) for x in combination))
+      interval += time.time() - subtract
+      subtract = time.time()
 
-      # Progress-uppdatering för att se till att datorn inte går sönder
       guesses += 1
-      if (time.time() - start) % 10 == 0:
-        print("Currently guessed " + str(guesses) + " passwords")
 
-      # Avbryt funktion om lösenord hittas
-      if hashed == hash:
-        print("Found password: " + "".join(str(x) for x in combination))
-        print("Passwords attempted: " + str(guesses))
-        end = time.time()
-        elapsed = end - start
-        print("Elapsed time: " + str(elapsed) + " seconds")
-        return
+      # Statusuppdatering för att se till att datorn inte går sönder
+      if interval >= 10:
+        print("Currently guessed " + str(guesses) + " passwords")
+        interval = 0
+
+      if hash_algo == "bcrypt":
+        candidate = "".join(str(x) for x in combination)
+        if bcrypt.checkpw(candidate.encode(), hash):
+          end(combination, guesses, start)
+          return
+      else:
+        hashed = hash_password("".join(str(x) for x in combination), hash_algo)
+        if hashed == hash:
+          end(combination, guesses, start)
+          return
       
   # Lösenord hittades inte
   print("Couldn't find password")
 
+def end(combination, guesses, start):
+  print("Found password: " + "".join(str(x) for x in combination))
+  print("Passwords attempted: " + str(guesses))
+  end = time.time()
+  elapsed = end - start
+  print("Elapsed time: " + str(elapsed) + " seconds")
   
 # Skicka in lösenord och kör brute force
-hash_type = hash_types[int(input("Hash type (sha256 <0>, bcrypt <1>, md5 <2>): "))]
+hash_algo = hash_types[int(input("Hash type (sha256 <0>, bcrypt <1>, md5 <2>): "))]
 password = input("Password: ")
-hash = hash_password(password)
+hash = hash_password(password, hash_algo)
 brute_force(hash, 94)
