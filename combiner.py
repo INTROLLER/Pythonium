42caabd09013d1a5aa61aca0baf5c8db4f597bb6
INTROLLER <pv.levap@gmail.com>
2026-02-06 11:28:48 +0100

    Update

--- combiner.py
+++ combiner.py
@@ -5,5 +5,5 @@ filename = "results/" + input("File name: ") + ".pkl"
 with open(filename, "rb") as f:
   data = pickle.load(f)
 
-with open("all.pkl", "wb") as f:
+with open("results/all.pkl", "wb") as f:
   pickle.dump(data, f)
